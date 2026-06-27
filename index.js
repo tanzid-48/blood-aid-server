6f9727b6e5821c3217f6da4138871263b940730a
tanzid-48 <mdtanzid.525@gmail.com>
2026-06-27 18:26:04 +0600

    Connecet to mongodb

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -12,8 +12,37 @@ app.get("/", (req, res) => {
   res.send("blood-aid-server is running ✅");
 });
 
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const authDb = client.db("blood_auth_db");
+    const db = client.db("blood-aid-db");
 
 
+
+
+    
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`🚀 Server running on port ${port}`);
